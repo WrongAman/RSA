565dff89ebf659c4b0d2a2cdf3a0746f2d824499
TakeArest <41245046+TakeArest@users.noreply.github.com>
2019-12-26 18:39:18 +0800

    完成genkyey窗口

--- newWindows.py
+++ newWindows.py
@@ -1,7 +1,18 @@
+# coding=utf-8
+
+import base64
+import sys
+import rsa
+import time,datetime
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5 as PKCS1_cipher
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QAction, QMessageBox, QDesktopWidget, QFileDialog, QApplication, QMainWindow, QTabWidget, \
     QListWidget, QHBoxLayout, QWidget, QPushButton, QLabel, QComboBox
 from PyQt5.Qt import QTextEdit
+from PyQt5.QtCore import QDateTime
+
 
 
 class genWindow(QWidget):
@@ -16,12 +27,14 @@ class genWindow(QWidget):
         self.genButton = QPushButton('Generate', self)
         self.public_button = QPushButton('Save', self)
         self.private_button = QPushButton('Save', self)
+        self.measure_time = ''
+        # 窗口不可放大和缩放
+        self.setFixedSize(500, 300)
+        
+
 
     def newWindowUI(self):
         self.setWindowTitle("Generate Key")
-
-        # 窗口
-        self.resize(500, 300)
 
         # 公钥文本框
         label1 = QLabel(self)
@@ -63,19 +76,43 @@ class genWindow(QWidget):
         self.private_button.resize(80, 30)
         self.private_button.clicked.connect(lambda: self.keySave('PrivateKey'))
 
-        # 保存方法
-        # def bt_save(self):
-        # filename=QFileDialog.getSaveFileName(self,'save file','C:\\')
-        # with open(filename[0],'w') as f:
-        # my_text=self.text.toPlainText()
-        # f.write(my_text)
-
+        # 生成时间计算
+        label3 = QLabel(self)
+        label3.setText('Elapsed Time:')
+        label3.move(405,170)
+        label3.resize(80,30)
+        self.label4 = QLabel(self)
+        self.label4.setFixedWidth(80)
+        self.label4.move(405,200)
+        
         self.show()
 
+    # 生成密钥
     def genKey(self):
-        self.publicTextbox.setPlainText('123')
-        self.privateTextbox.setPlainText('456')
+        # 时间戳开始时间
+        startTime = time.time()
+        n = int(self.cb.currentText())
+        rsa = RSA.generate(n,Random.new().read)
 
+        # 生成私钥
+        private_pem = rsa.exportKey()
+        private_key = private_pem.decode()
+        
+        # 生成公钥
+        public_pem = rsa.publickey().exportKey()
+        public_key = public_pem.decode()
+
+        # 显示公钥有和私钥
+        self.publicTextbox.setPlainText(public_key)
+        self.privateTextbox.setPlainText(private_key)
+        # 时间戳结束时间        
+        endTime = time.time()
+        self.measure_time = str(round((endTime - startTime),6))
+        self.label4.setText(self.measure_time + 's')
+
+        
+
+    # 保存密钥
     def keySave(self, fileName):
         if fileName.startswith('PublicKey'):
             file_txt = self.publicTextbox.toPlainText()
@@ -99,5 +136,9 @@ class genWindow(QWidget):
             msg.setWindowTitle('成功')
             msg.setText('生成密钥成功！')
             msg.exec_()
+
+
+
+            
 
 
