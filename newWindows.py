f70c52b82e0c1c57aa17f31ff4c4c3fcc4846bb7
TakeArest <41245046+TakeArest@users.noreply.github.com>
2019-12-28 09:13:32 +0800

    Update newWindows.py

--- newWindows.py
+++ newWindows.py
@@ -1,11 +1,13 @@
-
+import sys
 import time, datetime
 from Crypto import Random
 from Crypto.PublicKey import RSA
-from PyQt5.QtWidgets import QAction, QMessageBox, QDesktopWidget, QFileDialog, QApplication, QMainWindow, QTabWidget, \
-    QListWidget, QHBoxLayout, QWidget, QPushButton, QLabel, QComboBox
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QWidget, QPushButton, QLabel, QComboBox, QMessageBox, QTextEdit,\
+QLineEdit
 from PyQt5.Qt import QTextEdit
-
+import base64
+from PyQt5.QtCore import pyqtSignal
 
 class genWindow(QWidget):
 
@@ -20,8 +22,7 @@ class genWindow(QWidget):
         self.public_button = QPushButton('Save', self)
         self.private_button = QPushButton('Save', self)
         self.measure_time = ''
-        # 窗口不可放大和缩放
-        self.setFixedSize(500, 300)
+        
         self.newWindowUI()
 
     def newWindowUI(self):
@@ -76,6 +77,8 @@ class genWindow(QWidget):
         self.label4.setFixedWidth(80)
         self.label4.move(405, 200)
 
+        # 窗口不可缩放
+        self.setFixedSize(500, 300)
         self.show()
 
     # 生成密钥
@@ -86,16 +89,17 @@ class genWindow(QWidget):
         rsa = RSA.generate(n, Random.new().read)
 
         # 生成私钥
-        private_pem = rsa.exportKey()
-        private_key = private_pem.decode()
+        self.private_pem = rsa.exportKey()
+        private_key = self.private_pem.decode()
 
         # 生成公钥
-        public_pem = rsa.publickey().exportKey()
-        public_key = public_pem.decode()
+        self.public_pem = rsa.publickey().exportKey()
+        public_key = self.public_pem.decode()
 
         # 显示公钥有和私钥
         self.publicTextbox.setPlainText(public_key)
         self.privateTextbox.setPlainText(private_key)
+
         # 时间戳结束时间        
         endTime = time.time()
         self.measure_time = str(round((endTime - startTime), 6))
@@ -105,23 +109,88 @@ class genWindow(QWidget):
     def keySave(self, fileName):
         if fileName.startswith('PublicKey'):
             file_txt = self.publicTextbox.toPlainText()
+            key = self.public_pem
         if fileName.startswith('PrivateKey'):
             file_txt = self.privateTextbox.toPlainText()
+            key = self.private_pem
         if not file_txt:
             msg = QMessageBox()
             msg.setWindowTitle('提示')
             msg.setText('请先生成密钥！')
             msg.exec_()
-        else:
+        else: 
             filename, filetype = QFileDialog.getSaveFileName(self,
                                                              "保存文件",
                                                              fileName,
-                                                             "Text Files (*.txt)")
+                                                             "Text Files (*.pem)")
             if not filename:
                 return
-            with open(filename, 'w') as f:
-                f.write(file_txt)
+            with open(filename, 'wb') as f:
+                f.write(key)
             msg = QMessageBox()
             msg.setWindowTitle('成功')
             msg.setText('生成密钥成功！')
             msg.exec_()
+
+
+class cipherWindow(QWidget):
+    
+
+    def  __init__(self, text=''):
+        super().__init__()
+        self.text = text
+        # 窗口不可缩放
+        self.setFixedSize(500, 110)
+        self.cipherWindowUI()
+
+        
+
+    def cipherWindowUI(self):
+        self.setWindowTitle('Encode')
+        
+        # 导入公钥窗口
+        import_label = QLabel(self)
+        import_label.move(35, 20)
+        import_label.resize(175,20)
+        import_label.setText('Please import the public key:')
+        
+        # 显示路径
+        self.import_address = QLabel(self)
+        self.import_address.move(35,55)
+        self.import_address.resize(400,20)
+
+        # 导入公钥
+        import_button = QPushButton('import', self)
+        import_button.move(235, 20)
+        import_button.resize(45,20)
+        import_button.clicked.connect(self.uploadKey)
+        
+        # 加密
+        encode_button = QPushButton('encode', self)
+        encode_button.move(300,20)
+        encode_button.resize(45,20)
+        encode_button.clicked.connect(self.rsaEncode)
+        
+        self.show()
+
+    # 导入pem文件    
+    def uploadKey(self):
+        file_name, filetype = QFileDialog.getOpenFileName(self,
+                                                             "打开文件",
+                                                             "C://",
+                                                              "Text Files (*.pem)")
+        self.import_address.setText(file_name)
+        if file_name == "":
+            return 
+        with open(file_name, 'r') as f:
+            self.key = f.read()
+            self.rsakey = RSA.importKey(self.key)
+            self.cipher = Cipher_pkcs1_v1_5.new(self.rsakey)
+        
+    def rsaEncode(self):
+        self.mySignal = pyqtSignal()
+        message = self.text
+        self.cipher_text = base64.b64encode(self.cipher.encrypt(message.encode(encoding="utf-8")))
+        print(self.cipher_text)
+        self.mySignal.emit("hello")
+        self.close()
